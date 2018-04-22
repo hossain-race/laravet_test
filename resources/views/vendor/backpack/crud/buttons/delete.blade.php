9460c3f9fbf60b797a817fe59190df750c1b6329
Enayet <seacse05@gmail.com>
2018-04-22 17:14:38 +0600

    delete action

--- resources/views/vendor/backpack/crud/buttons/delete.blade.php
+++ resources/views/vendor/backpack/crud/buttons/delete.blade.php
@@ -1,6 +1,6 @@
 @if ($crud->hasAccess('delete'))
 	@if ($crud->route == 'admin/product')
-		<a href="{{ url($crud->route.'/delete'.$entry->getKey()) }}" class="btn btn-xs btn-default" data-button-type="delete"><i class="fa fa-trash"></i> {{ trans('backpack::crud.delete') }}</a>
+		<a href="{{ url($crud->route.'/delete/'.$entry->getKey()) }}" class="btn btn-xs btn-default" data-button-type="delete"><i class="fa fa-trash"></i> {{ trans('backpack::crud.delete') }}</a>
 	@else
 		<a href="{{ url($crud->route.'/'.$entry->getKey()) }}" class="btn btn-xs btn-default" data-button-type="delete"><i class="fa fa-trash"></i> {{ trans('backpack::crud.delete') }}</a>
 
